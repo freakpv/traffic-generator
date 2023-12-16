93bee4afc3f77d52a2d522e59d09cb430970323b
Pavel Vazharov <pavel@x3me.net>
2023-12-16 19:34:59 +0200

    Add some more application logic

--- aliases.h
+++ aliases.h
@@ -3,6 +3,19 @@
 namespace stdex = std::experimental;
 namespace stdfs = std::filesystem;
 
+namespace bout = boost::outcome_v2;
 namespace bpo  = boost::program_options;
 namespace bsys = boost::system;
 namespace bx3  = boost::spirit::x3;
+
+////////////////////////////////////////////////////////////////////////////////
+// Some common types and functions used throughout the project
+namespace tglib
+{
+
+inline bsys::error_code system_error_code(int ec) noexcept
+{
+    return {ec, bsys::system_category()};
+}
+
+} // namespace tglib
--- application.cpp
+++ application.cpp
@@ -2,7 +2,35 @@
 
 #include "cfg/config.h"
 #include "log/log.h"
+
+#include "put/file_utils.h"
 #include "put/throw.h"
+
+namespace
+{
+
+void ensure_enough_filedesc()
+{
+    // When creating the DPDK mbufs pool the DPDK framework opens file
+    // descriptors for every huge page which is part of the pool. It does this
+    // for the virtual to physical mapping machinery.
+    // Here we just ensure that there are enough file descriptors even for
+    // very large amount of huge pages.
+    // 65536 * 2 MB huge page size ensures that the application will be able
+    // to open file descriptors for almost 128 GB huge pages which is more than
+    // it'll even need in practice.
+    constexpr auto cnt_fds = 64 * 1024uz;
+    if (auto res = put::set_max_count_fds(cnt_fds); !res) {
+        put::throw_system_error(
+            res.error(),
+            "Failed to set the max count of file descriptors to {}!", cnt_fds);
+    }
+    TGLOG_INFO("Set max file descriptors to {}\n", cnt_fds);
+}
+
+} // namespace
+
+////////////////////////////////////////////////////////////////////////////////
 
 class application_impl
 {
@@ -17,10 +45,10 @@ application::application(const stdfs::path& cfg_path)
     cfg::config cfg(cfg_path);
     try {
         TGLOG_INFO("Starting with with settings: {}\n", cfg);
-        // init_max_count_filedesc(cfg);
+        ensure_enough_filedesc();
         // dpdk_eal_init(cfg);
-        // xlib::scope_guard cleanup([] { rte_eal_cleanup(); });
-        // impl_ = mem::make_unique<application_impl>(cfg);
+        stdex::scope_fail cleanup([] { rte_eal_cleanup(); });
+        impl_ = std::make_unique<application_impl>(cfg);
     } catch (const std::exception& ex) {
         put::throw_runtime_error("Can not construct Traffic-Generator. {}",
                                  ex.what());
