d84137640609462f1e7bac283642989d3ce7126b
Pavel Vazharov <pavel@x3me.net>
2023-12-29 18:58:34 +0200

    Inline put::time_utils functions

--- put/time_utils.h
+++ put/time_utils.h
@@ -3,22 +3,22 @@
 namespace put
 {
 
-uint64_t duration_to_tsc(stdcr::seconds dur) noexcept
+inline uint64_t duration_to_tsc(stdcr::seconds dur) noexcept
 {
     return dur.count() * rte_get_tsc_hz();
 }
 
-uint64_t duration_to_tsc(stdcr::milliseconds dur) noexcept
+inline uint64_t duration_to_tsc(stdcr::milliseconds dur) noexcept
 {
     return (dur.count() * rte_get_tsc_hz()) / 1'000ul;
 }
 
-uint64_t duration_to_tsc(stdcr::microseconds dur) noexcept
+inline uint64_t duration_to_tsc(stdcr::microseconds dur) noexcept
 {
     return (dur.count() * rte_get_tsc_hz()) / 1'000'000ul;
 }
 
-uint64_t duration_to_tsc(stdcr::nanoseconds dur) noexcept
+inline uint64_t duration_to_tsc(stdcr::nanoseconds dur) noexcept
 {
     return (dur.count() * rte_get_tsc_hz()) / 1'000'000'000ul;
 }
