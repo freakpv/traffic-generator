678c2d4f0a00a8449dd56577a68caf96b512195b
Pavel Vazharov <pavel@x3me.net>
2023-12-30 11:53:34 +0200

    Rename gen::priv::time_event_scheduler to gen::priv::event_scheduler

---
+++ gen/priv/event_handle.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace gen::priv
+{
+
+class event_handle
+{
+    // TODO:
+public:
+    event_handle() noexcept = default;
+};
+
+}; // namespace gen::priv
--- gen/priv/event_scheduler.h
+++ gen/priv/event_scheduler.h
@@ -1,14 +1,38 @@
 #pragma once
 
+#include "put/time_utils.h"
+
 namespace gen::priv
 {
 
-// Decouple the interface for (re)scheduling events from the actual
-// implementation
 class event_scheduler
 {
+    // We need precision of 1 microsecond because:
+    // - this is the precision of the PCAP timestamps
+    // - this is the precision that we allow for the inter packet gaps
+    const uint64_t usec_ticks_ = put::duration_to_tsc(stdcr::microseconds{1});
+    uint64_t prev_ticks_       = 0;
+
 public:
-    virtual ~event_scheduler() noexcept = default;
+    event_scheduler() noexcept { rte_timer_subsystem_init(); }
+    ~event_scheduler() noexcept { rte_timer_subsystem_finalize(); }
+
+    event_scheduler(event_scheduler&&)                 = delete;
+    event_scheduler(const event_scheduler&)            = delete;
+    event_scheduler& operator=(event_scheduler&&)      = delete;
+    event_scheduler& operator=(const event_scheduler&) = delete;
+
+    void process_events() noexcept
+    {
+        if (const auto ticks = rte_get_tsc_cycles();
+            (ticks - prev_ticks_) >= usec_ticks_) {
+            prev_ticks_ = ticks;
+            rte_timer_manage();
+        }
+    }
+
+    // TODO:
+    size_t count_events() const noexcept { return 0; }
 };
 
 } // namespace gen::priv
